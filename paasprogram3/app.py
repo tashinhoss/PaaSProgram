263f232b3d9f0b31354bd04a116d60830a8b1c0a
tashinhoss <82747136+tashinhoss@users.noreply.github.com>
2023-06-12 15:03:56 +0600

    Update app.py

--- paasprogram3/app.py
+++ paasprogram3/app.py
@@ -1,33 +1,63 @@
-from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+# Database connection
+conn = sqlite3.connect('login.db')
+cursor = conn.cursor()
+
+# Create the users table if it doesn't exist
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+''')
+conn.commit()
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
-        user = User.query.filter_by(username=username, password=password).first()
-        if user:
-            return redirect('/dashboard')
-        else:
-            return render_template('index.html', message='Invalid credentials. Please try again.')
-    
-    return render_template('index.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+        # Check if the username and password match in the database
+        cursor.execute('SELECT * FROM users WHERE username=? AND password=?', (username, password))
+        user = cursor.fetchone()
+
+        if user:
+            return redirect(url_for('home'))
+        else:
+            return render_template('login.html', error='Invalid username or password')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username already exists in the database
+        cursor.execute('SELECT * FROM users WHERE username=?', (username,))
+        existing_user = cursor.fetchone()
+
+        if existing_user:
+            return render_template('register.html', error='Username already exists')
+
+        # Insert the new user into the database
+        cursor.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password))
+        conn.commit()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return 'Welcome to the home page'
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
